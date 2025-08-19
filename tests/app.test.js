ec5fd656ffe8c023640c99e120aeafb45f211988
sidpd51 <sidpd510@gmail.com>
2025-08-19 21:23:30 +0530

    feat: Add test case for post api

--- tests/app.test.js
+++ tests/app.test.js
@@ -13,3 +13,17 @@ describe("GET /", () => {
 		expect(res.text).not.toBe("Hello World!");
 	});
 });
+
+describe("POST /", () => {
+	it("should return 200 and Hola, Mohit when name query param is provided", async () => {
+		const res = await request(app).post("/").query({ name: "Mohit" });
+		expect(res.statusCode).toBe(200);
+		expect(res.text).toBe("Hola, Mohit");
+	});
+
+	it("should return 200 and Hola, undefined when no query param is provided", async () => {
+		const res = await request(app).post("/");
+		expect(res.statusCode).toBe(200);
+		expect(res.text).toBe("Hola, undefined");
+	});
+});
